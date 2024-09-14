ae5e4e263e138011c21a5a7c316e49df0a217b75
GianNP <93480519+GianNP@users.noreply.github.com>
2024-09-14 14:01:21 +0700

    Nambah include header footer

---
+++ partials/header.php
@@ -0,0 +1,49 @@
+<?php
+require 'config/database.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda | SmeArt</title>
+    <!-- CSS -->
+     <link rel="stylesheet" href="style.css">
+    <!-- Iconscout -->
+     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
+     <!-- Poppins Font -->
+     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!-- Navbar -->
+    <nav>
+        <div class="container nav__container">
+            <div class="nav__logo">
+                <h1>SmeArt</h1>
+            </div>
+            <ul class="nav__items">
+                <li><a href="blog.php">Blog</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <!-- <li><a href="login.php">Login</a></li> -->
+                <li class="nav__profile">
+                    <div class="avatar">
+                        <img src="img/assets/avatar0.jpg">
+                    </div>
+                    <ul>
+                        <li><a href="/SmeArt/admin/dashboard.php">Dashboard</a></li>
+                        <li><a href="logout.php">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+
+            <button id="open__nav-btn"><i class="uil uil-bars"></i></button>
+            <button id="close__nav-btn"><i class="uil uil-multiply"></i></button>
+        </div>
+    </nav>
+    <!-- End Navbar -->
+
+    <script src="js/main.js"></script>
