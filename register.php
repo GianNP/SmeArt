4c8c74e4191489459b38742855231ff80f48fced
GianNP <93480519+GianNP@users.noreply.github.com>
2024-09-22 14:00:53 +0700

    Update register.php

--- register.php
+++ register.php
@@ -76,7 +76,7 @@ require 'config/constans.php';
 <div class="signup-container">
     <img src="img/logo.png" alt="Instagram Logo" width="180px">
     <h2>Daftar untuk melihat apa yang terjadi di SMAKANZA.</h2>
-    <form action="<?ROOT_URL?>register-logic.php" method="post" enctype="multipart.form-data">
+    <form action="<?= ROOT_URL ?>register-logic.php" method="post" enctype="multipart.form-data">
         <input type="text" name="firstname" placeholder="Nama Depan" required>
         <input type="text" name="lastname" placeholder="Nama Belakang" required>
         <input type="text" name="username" placeholder="Username" required>
